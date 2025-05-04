76ce7d0822eee865d0464b44ad283bc66a99530d
csprolesUNCC <csproles@charlotte.edu>
2025-05-04 13:00:58 -0400

    Added dynamic footer

--- itis3135/project/scripts/layout.js
+++ itis3135/project/scripts/layout.js
@@ -5,3 +5,15 @@ function loadHeader() {
       document.getElementById("dynamic-header").innerHTML = html;
     });
 }
+
+function loadFooter() {
+  fetch("components/footer.html")
+    .then((res) => res.text())
+    .then((data) => {
+      const footer = document.getElementById("dynamic-footer");
+      if (footer) {
+        footer.innerHTML = data;
+      }
+    })
+    .catch((err) => console.error("Footer load failed:", err));
+}
